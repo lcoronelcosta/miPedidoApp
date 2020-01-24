20260e10e67e7d7c3ca775030e6de3763fe8f603
lcoronelcosta <lufecoro@outlook.com>
2020-01-24 06:31:32 -0500

    Carrito

--- www/js/consultas.js
+++ www/js/consultas.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	localStorage.clear();
+	window.localStorage.clear();
 	/*var useragent = navigator.userAgent;
 	var secretKey = 'ABCDEF123456';
 	var f = new Date();
@@ -154,7 +154,7 @@ function getProductos(id){
 												
 												<div class="product-btns">
 													<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">Favorito</span></button>
-													<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">Mas info</span></button>
+													<button class="quick-view" onclick="verProducto(${dato.id});"><i class="fa fa-eye"></i><span class="tooltipp">Mas info</span></button>
 												</div>
 											</div>
 											<div class="add-to-cart">
@@ -183,13 +183,19 @@ function getProducto(id){
 	})
 
 	.then(function(dato) {
-		var cantidad = aumentarCarrito();
-		var subtotal = aumentarSubtotal(dato.precio);
-		console.log(subtotal);
+		var cantidad = aumentarCarrito(dato);
+		//var subtotal = aumentarSubtotal(dato.precio);
 		$('#qty').html(cantidad);
 		$('#item-seleccionados').html(cantidad + ' Item(s) selected');
-		$('#subtotal-items').html('SUBTOTAL: $' + subtotal);
+		//$('#subtotal-items').html('SUBTOTAL: $' + subtotal);
 		const contenedor = document.getElementById('cart-list');
+
+		$.each(localStorage, function(key, value){
+			console.log(JSON.parse(value));
+		  //console.log('objeto: ', JSON.parse(value));
+
+		});
+
 		//myJson.data.map((dato) => {
             contenedor.innerHTML += `<div class="product-widget" id="producto-${dato.id}">
             							<input type="hidden" value="${dato.precio}" id="precio-${dato.id}" />
@@ -200,7 +206,7 @@ function getProducto(id){
 									        <h3 class="product-name"><a href="#">${dato.nombre}</a></h3>
 									        <h4 class="product-price"><span class="qty">1x</span>${dato.precio}</h4>
 									    </div>
-									    <button class="delete" onclick="deleteProductoCart(${dato.id}, ${dato.precio})"><i class="fa fa-close"></i></button>
+									    <button onclick="deleteProductoCart(${dato.id}, ${dato.precio})" class="delete" "><i class="fa fa-close"></i></button>
 									</div>`;
         //});
 
@@ -209,48 +215,56 @@ function getProducto(id){
 
 }
 
-function deleteProductoCart(id){
+function deleteProductoCart(id, precio){
 	console.log(id);
-	var cantidad_actual = parseInt(localStorage.getItem("cantidad"));
+	var cantidad_actual = parseInt(window.localStorage.getItem("cantidad"));
 	var total = cantidad_actual - 1;
-	localStorage.setItem("cantidad", total);
-	var cantidad = parseInt(localStorage.getItem("cantidad"));
+	window.localStorage.setItem("cantidad", total);
+
+	var cantidad = parseInt(window.localStorage.getItem("cantidad"));
 	$("#producto-"+id).remove();
 	$('#qty').html(cantidad);
 	$('#item-seleccionados').html(cantidad + ' Item(s) selected');
 
-	var total = parseFloat(localStorage.getItem("subtotal")) - parseFloat($("#precio-"+id).val());
-	localStorage.setItem("subtotal", total);
+	var total = parseFloat(window.localStorage.getItem("subtotal")) - parseFloat($("#precio-"+id).val());
+	window.localStorage.setItem("subtotal", total);
 	$('#subtotal-items').html('SUBTOTAL: $' + total);
 	
 }
 
-function aumentarCarrito(){
-	if (localStorage.getItem("cantidad") === null) {
-			localStorage.setItem("cantidad", 1);
+function aumentarCarrito(dato){
+	window.localStorage.setItem(dato.id, JSON.stringify(dato));
+	var guardado = localStorage.getItem(dato.id);
+
+
+
+	console.log('objetoObtenido: ', JSON.parse(guardado));
+
+	if (window.localStorage.getItem("cantidad") === null) {
+			window.localStorage.setItem("cantidad", 1);
 	}
 	else{
-		var cantidad_actual = parseInt(localStorage.getItem("cantidad"));
+		var cantidad_actual = parseInt(window.localStorage.getItem("cantidad"));
 		var total = cantidad_actual + 1;
-		localStorage.setItem("cantidad", total);
+		window.localStorage.setItem("cantidad", total);
 	}
-	var cantidad = parseInt(localStorage.getItem("cantidad"));
+	var cantidad = parseInt(window.localStorage.getItem("cantidad"));
 	return cantidad;
 }
 
-function aumentarSubtotal(precio){
+/*function aumentarSubtotal(precio){
 	console.log(precio);
-	if (localStorage.getItem("subtotal") === null) {
-		localStorage.setItem("subtotal", precio);
+	if (window.localStorage.getItem("subtotal") === null) {
+		window.localStorage.setItem("subtotal", precio);
 	}
 	else{
-		var subtotal_actual = parseFloat(localStorage.getItem("subtotal"));
+		var subtotal_actual = parseFloat(window.localStorage.getItem("subtotal"));
 		var total = subtotal_actual + parseFloat(precio);
-		localStorage.setItem("subtotal", total);
+		window.localStorage.setItem("subtotal", total);
 	}
-	var subtotal = parseFloat(localStorage.getItem("subtotal"));
+	var subtotal = parseFloat(window.localStorage.getItem("subtotal"));
 	return subtotal;
-}
+}*/
 
 					
 function generaQR(){
@@ -258,6 +272,80 @@ function generaQR(){
 		jQuery('#categorias').qrcode("http://espol.edu.ec");
 	})
 }
+
+
+function verProducto(id){
+	var myHeaders = new Headers();
+	var miInit = { method: 'GET',
+               mode: 'cors',
+               headers: myHeaders,
+               cache: 'default' };
+
+	fetch('http://hierrodiseno.com/mipedido/public/api/getproducto?id='+id,miInit)
+	.then(function(response) {
+    return response.json();
+	})
+
+	.then(function(dato) {
+		var cantidad = aumentarCarrito(dato);
+		var subtotal = aumentarSubtotal(dato.precio);
+		console.log(subtotal);
+		$('#qty').html(cantidad);
+		$('#item-seleccionados').html(cantidad + ' Item(s) selected');
+		$('#subtotal-items').html('SUBTOTAL: $' + subtotal);
+		$('#categorias').html("");
+		const contenedor = document.getElementById('categorias');
+		//myJson.data.map((dato) => {
+            contenedor.innerHTML += 
+            `<div class="col-md-5 col-md-push-0">
+							<div id="product-main-img">
+								<div class="product-preview">
+									<img src="${dato.imagen}" alt="">
+								</div>
+							</div>
+			</div>
+			<!-- /Product main img -->
+
+
+			<!-- Product details -->
+			<div class="col-md-5">
+				<div class="product-details">
+					<h2 class="product-name">${dato.nombre}</h2>
+					<div>
+						<h3 class="product-price">${dato.precio}</h3>
+						<span class="product-available">In Stock</span>
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+					<div class="add-to-cart">
+						<div class="qty-label">
+							Qty
+							<div class="input-number">
+								<input type="number">
+								<span class="qty-up">+</span>
+								<span class="qty-down">-</span>
+							</div>
+						</div>
+						<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+					</div>
+
+				</div>
+			</div>
+			<!-- /Product details -->
+
+		</div>`;
+        //});
+
+	    console.log(dato);
+	});
+}
+
+
+
+
+					
+
+
 
 
 
